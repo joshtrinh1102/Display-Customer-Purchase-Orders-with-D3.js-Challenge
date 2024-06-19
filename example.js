5592c09a98154f7b00e11c3d335fcd1ed8c59f55
joshtrinh1102 <nltrinh1102@gmail.com>
2024-06-18 21:32:25 -0400

    Add sample CSV data file

---
+++ example.js
@@ -0,0 +1,10 @@
+function main() {
+    var myData= d3.csv('purchase_orders.csv', d3.autoType)
+    d3.csv('purchase_orders.csv', d3.autoType).then(
+        function(){
+            d3.select("ul")
+            .append('li')
+            
+        }
+    )
+}
